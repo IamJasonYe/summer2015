ff873cf5f71fbc53351fe47ef236fab0d6d0087b
JasonYe <jason421202795@gmail.com>
2015-09-10 07:41:13 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+#coding=utf-8
+import time, os, sched, sys
+import drive_mario as dm
+#第一个参数确定任务的时间，返回某个特定的时间到现在的秒数
+#第二个参数以某种人为的方式衡量时间
+schedule = sched.scheduler(time.time, time.sleep)
+nTime = 1
+def perform_command(cmd, inc):
+    # 安排inc秒后再次运行自己，即周期运行
+    global nTime
+    schedule.enter(inc, 0, perform_command, (cmd,inc))
+    #os.system(cmd)
+    #print(time.time())
+    print 'execute time:' + str(time.time()), 'at nTIme:' + str(nTime)
+    nTime = nTime + 1
+# 读命令
+   # mario_com = sys.stdin.readline()
+    mario_com = 'p'
+    print mario_com
+    dm.send_command_to_mario(arduino, mario_com)
+    time.sleep(1)
+    mario_resp = dm.read_response_from_mario(arduino)
+    print mario_resp
+def timming_exe(cmd, inc = 60):
+    # enter用来安排某事件的发生时间,从现在起第n秒开始启动
+    schedule.enter(inc, 0, perform_command, (cmd, inc))
+    # 持续运行，直到计划时间队列变成空为止
+    schedule.run()
+
+if __name__ == '__main__':
+#   tt=time.time()
+#   print('show time after 5 seconds:', tt)
+    arduino = dm.open_arduino_port()
+    timming_exe('echo %time%', 2)
+
