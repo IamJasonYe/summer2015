1ce916ffe4bfb89986eaeddaca3a28e0fbd48d8f
JasonYe <jason421202795@gmail.com>
2015-09-13 14:01:14 +0800

    new framework for arduino code

---
+++ arduino/mario/mario.ino
@@ -0,0 +1,15 @@
+#include "port_def.h"
+#include "motor_control.h"
+#include "ultrasound.h"
+#include "gripper.h"
+//#include <Servo.h>
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
---
+++ arduino/mario/port_def.h
@@ -0,0 +1,25 @@
+/* This file is for Port definition */
+/*
+Author: Jason Ye
+Tiem  ：2015/09/13
+*/
+#ifndef PORT_DEF
+#define POrT_DEF 
+
+//Car control port
+#define IN1 5
+#define IN2 6
+#define IN5 3
+#define IN6 11
+//Servo port
+#define GripperServoPin 4
+
+//Gripper default positions
+#define Gopen 800
+#define Gclose 2200
+
+//Ultarsound port
+#define triggerPort A1  //Trigger Pin
+#define echoPort    A2  //Echo Pin
+
+#endif
