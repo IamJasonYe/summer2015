7a197443bdb7f950bb5c7636785b10fc42bc0526
JasonYe <jason421202795@gmail.com>
2015-09-13 07:59:46 +0000

    is your turn now

--- pi/ultrasound_pub.py
+++ pi/ultrasound_pub.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
     socket.bind("tcp://*:5557")
+    dis = [0, 0, 0, 0, 0]
     while True:
         mario_com = 'p'
         dm.send_command_to_mario(arduino, mario_com)
@@ -13,11 +14,15 @@ if __name__ == '__main__':
         mario_resp = dm.read_response_from_mario(arduino)
         distance = mario_resp.split(':')
         if(len(distance)>1):
-            distance = '%i' % int(distance[1])
+            dis.pop()
+            dis.insert(int(distance[1]))
+            distance = str(mid_filter(dis))
+            #distance = '%i' % int(distance[1])
             outString = 'd:' + str(distance)
         else:
             distance = 3456
             outString = 'd:'
         socket.send_string(outString)
-        print(outString)
+        #print(outString)
+        print(mario_resp)
         time.sleep(0.23)
